afe6c5ffaea36694685c92d76545726c1f1994ae
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-20 01:14:15 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+
+    document.getElementById("create-task-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    event.target[0].value;
+    
+    let li = document.createElement("li");
+    
+    li.textContent = event.target[0].value;
+    
+    document.getElementById("tasks").append(li)
+    
+    });
+    
+  })
